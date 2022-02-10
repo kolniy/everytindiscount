ce0a7e895c1c6feab4873103b2f0e77f04da026e
kolawole olaniyi <kolaniyi3@gmail.com>
2022-02-10 21:11:08 +0100

    fixed bug with frontend wss connection

--- client/src/service/globals.js
+++ client/src/service/globals.js
@@ -1,2 +1,2 @@
 export const url = process.env.NODE_ENV === "production" ? "https://everytindiscount.herokuapp.com/graphql" : "http://localhost:4000/graphql"
-export const wsurl = process.env.NODE_ENV === "production" ? "ws://everytindiscount.herokuapp.com/graphql" : "ws://localhost:4000/graphql"
+export const wsurl = process.env.NODE_ENV === "production" ? "wss://everytindiscount.herokuapp.com/graphql" : "ws://localhost:4000/graphql"
