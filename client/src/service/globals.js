311aa04bea235c6c00a43d202ddef0661ce72476
kolawole olaniyi <kolaniyi3@gmail.com>
2023-06-29 14:35:29 +0100

    updated production url in globals.js file for client

--- client/src/service/globals.js
+++ client/src/service/globals.js
@@ -1,2 +1,8 @@
-export const url = process.env.NODE_ENV === "production" ? "https://everytindiscount.herokuapp.com/graphql" : "http://localhost:4000/graphql"
-export const wsurl = process.env.NODE_ENV === "production" ? "wss://everytindiscount.herokuapp.com/graphql" : "ws://localhost:4000/graphql"
+export const url =
+  process.env.NODE_ENV === "production"
+    ? "https://everytindiscount.onrender.com/graphql"
+    : "http://localhost:4000/graphql";
+export const wsurl =
+  process.env.NODE_ENV === "production"
+    ? "wss://everytindiscount.onrender.com/graphql"
+    : "ws://localhost:4000/graphql";
